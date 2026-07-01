594546b8798fa507100feb25d789aa5995b6d689
Sabari M <sabarimanickarj269@gmail.com>
2026-07-01 11:25:44 +0530

    chore: initial project setup from Spring Initializr

---
+++ Employee-task-mgmt/src/main/java/com/assignment/Employee_task_mgmt/EmployeeTaskMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.Employee_task_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTaskMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTaskMgmtApplication.class, args);
+	}
+
+}
---
+++ Employee-task-mgmt/src/test/java/com/assignment/Employee_task_mgmt/EmployeeTaskMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.Employee_task_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTaskMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
